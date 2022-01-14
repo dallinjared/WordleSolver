a8ac7b27bc070b6b636a5f5c28b21b0aa53c912f
dallinjared <djared@avolta.us>
2022-01-14 13:54:18 -0700

    Update case handling and edge case situations

--- LetterOccurenceByPosition.py
+++ LetterOccurenceByPosition.py
@@ -4,13 +4,13 @@ file = open('Data/words.json')
 data = json.load(file)
 
 def getPercentage(position, total):
-    return round(((position/total) * 100), 2)
+    if position <= 0 or total <= 0 :
+        return 0.0
+    else :
+        return round(((float(position)/total) * 100), 2)
 
 def getOccurenceByPosition(wordList, fileName):
-    #For each Letter in alphabet
-    #from 0-4 (1-5) check string at index for that letter
-    #sum count and append to dictionary for that letter with the key being the letter
-    #occurenceByPosition = ["y": {'totalOccurence': 425, 'letter1': 25, 'letter2': 75, 'letter3': 25, 'letter4': 25, 'letter5': 275}]
+    
     alphabet = ["a", "b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
     occurenceByPosition = {}
